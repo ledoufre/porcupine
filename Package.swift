9d8e3d414e59b688b1774827b23929888cc2f0c6
Fred RAJAONA <fred@kannelle.io>
2022-01-10 22:12:12 +0100

    feat(SPM): implement SPM support

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "porcupine",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "porcupine",
+            targets: ["porcupine"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/ledoufre/ios-voice-processor.git", .branch("feat-spm-support")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "porcupine",
+            dependencies: ["ios-voice-processor", "PvPorcupine"],
+            path: "binding/ios",
+            exclude: ["Porcupine-iOS.podspec", "README.md"]),
+        .binaryTarget(
+            name: "PvPorcupine",
+            path: "lib/ios/PvPorcupine.xcframework"
+        )
+    ]
+)
